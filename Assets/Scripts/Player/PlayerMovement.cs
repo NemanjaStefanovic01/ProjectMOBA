267c30ef8f6c73daaf0dc97739cf63c3a71ecabb
Educons <educons@educons.educons>
2024-03-07 17:38:05 +0100

    Initial

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public string terrainTag = "Terrain";
+    public float movementSpeed = 5f;
+
+    public bool canMove = true;
+
+    private Vector3 targetPosition;
+    private bool isMoving = false;
+
+    void Update()
+    {
+        HandleInput();
+
+        if(canMove)
+            MoveCharacter();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, Mathf.Infinity))
+            {
+                //Ako pogodi teren
+                if (hit.collider.CompareTag(terrainTag))
+                {
+                    targetPosition = hit.point + Vector3.up; // Vector3.up -> doda +1 na y
+                    isMoving = true;
+                }
+
+                //Ako pogodi protivnika ide u combat scriptu
+            }
+        }
+    }
+    private void MoveCharacter()
+    {
+        if (isMoving)
+        {
+            Vector3 direction = (targetPosition - transform.position).normalized;
+            float distance = Vector3.Distance(transform.position, targetPosition); //.Distance vraca distancu izmedju 2 Vec
+
+            if (distance > 0.1f)
+            {
+                transform.position += direction * movementSpeed * Time.deltaTime;
+            }
+            else
+            {
+                isMoving = false;
+            }
+        }
+    }
+}
